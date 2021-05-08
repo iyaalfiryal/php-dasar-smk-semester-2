7118aae5bc86d39b05ce3c97d9017614e4563f46
iyaalfiryal <fifihuwaida@gmail.com>
2021-05-08 08:07:22 +0700

    sabtupagi

---
+++ Perulangan/ForEach.php
@@ -0,0 +1,17 @@
+<?php
+$names = ["Elina", "Hasna", "Zahra"];
+
+//tanpa for each
+// for ($i = 0; $i < count($names); $i++) {
+//     echo "Data ke $i = $names[$i]" . PHP_EOL;
+// }
+
+// //pake for each cetak tanpa index
+// foreach ($names as $name) {
+//     echo "Data $name" . PHP_EOL;
+// }
+
+// //pake for each cetak dengan index
+// foreach ($names as $index => $name) {
+//     echo "Data ke $index = $name" . PHP_EOL;
+// }
